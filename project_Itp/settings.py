c24929674d54dbfe4c642b90ba44280b516ede40
Mansoor <63596883+mansoorgt@users.noreply.github.com>
2023-05-15 17:13:42 +0530

    Update settings.py

--- project_Itp/settings.py
+++ project_Itp/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = '/registration/registration/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
