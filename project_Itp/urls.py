a909a52c4ae67e5d0be2d7202d2b3b52a3ac7b7d
Mansoor <63596883+mansoorgt@users.noreply.github.com>
2023-05-15 19:12:23 +0530

    Update urls.py

--- project_Itp/urls.py
+++ project_Itp/urls.py
@@ -18,9 +18,9 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('registration/admin/', admin.site.urls),
     path('registration/',include('App.urls')),
-    path('portal/',include('Portal.urls'))
+    path('registration/portal/',include('Portal.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
